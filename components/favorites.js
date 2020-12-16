ce5f1fa6126d1841d3b37b6e805f36fe94a98683
Paul Park <tue63405@temple.edu>
2020-12-16 18:22:47 -0500

    added scrollview

--- components/favorites.js
+++ components/favorites.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Button, StyleSheet, Linking, Alert } from 'react-native'
+import { View, Text, Button, StyleSheet, Linking, Alert, ScrollView } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default class Favorites extends Component {
@@ -66,19 +66,23 @@ export default class Favorites extends Component {
       if (this.state.locations !== null) {
         return (
           <View style={styles.container}>
+            <ScrollView>
             {this.state.locations.map((name, index) => {
               return <View key={index}>
                       <Text style={styles.text}>{name}</Text>
-                      <Button 
-                        title='Go'
-                        onPress={() => this.navToLoc(name)}
-                      />
-                      <Button 
-                        title='Delete'
-                        onPress={() => this.removeLoc(name)}
-                      />
-                    </View>
+                        <View style={styles.fix}>
+                          <Button 
+                            title='Go'
+                            onPress={() => this.navToLoc(name)}
+                          />
+                          <Button 
+                            title='Delete'
+                            onPress={() => this.removeLoc(name)}
+                          />
+                        </View>
+                      </View>
             })}
+            </ScrollView>
           </View>
         )
       }
@@ -89,9 +93,17 @@ export default class Favorites extends Component {
     container: {
       flex: 1,
       justifyContent: 'center',
-      margin: '10%'
+      paddingLeft: '10%',
+      paddingTop: '20%',
+      paddingRight: '10%'
     },
     text: {
-      textAlign: 'left'
+      textAlign: 'center'
+    },
+    fix: {
+      flexDirection: 'row',
+      justifyContent: 'space-around'
     }
  })
+
+ 
